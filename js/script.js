1eabfb9f0ef01a8609a59473fc19e77508f578a4
angelali92 <92.angelali@gmail.com>
2015-01-31 10:11:07 -0800

    changed layout of skeleton - buttons

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 var app = angular.module("heyScheduleMe", ["firebase"]);
 
 app.controller("SampleCtrl", function($scope, $firebase) {
-  var ref = new Firebase("https://heyscheduleme.firebaseio.com/messages");
+  var ref = new Firebase("https://heyscheduleme.firebaseio.com/events");
   var sync = $firebase(ref);
 
   $scope.data = {};
   $scope.data.text = "test";
 
-  $scope.messages = sync.$asArray();
+  $scope.events = sync.$asArray();
 
-  $scope.addMessage = function(text) {
-    $scope.messages.$add({text: text});
+  $scope.addEvent = function(text) {
+    $scope.events.$add({text: text});
   }
 });
