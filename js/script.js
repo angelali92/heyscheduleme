5604c80f94c87400206f4556e1739bde3becaeee
angelali92 <92.angelali@gmail.com>
2015-01-31 13:05:02 -0800

    still working on upcoming events

--- js/script.js
+++ js/script.js
@@ -4,12 +4,11 @@ app.controller("SampleCtrl", function($scope, $firebase) {
   var ref = new Firebase("https://heyscheduleme.firebaseio.com/events");
   var sync = $firebase(ref);
 
-  $scope.data = {};
-  $scope.data.text = "test";
-
   $scope.events = sync.$asArray();
+  $scope.predicate = 'time';
 
   $scope.addEvent = function(name, location, time) {
     $scope.events.$add({name: name, location: location, time: time});
-  }
+  };
+
 });
