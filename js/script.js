019f085efd35d1f9ddc653dcfc5b4faaf2033f22
angelali92 <92.angelali@gmail.com>
2015-01-31 14:15:57 -0800

    tracking upcoming event

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 var app = angular.module("heyScheduleMe", ["firebase"]);
 
 app.controller("SampleCtrl", function($scope, $firebase) {
-  var ref = new Firebase("https://heyscheduleme.firebaseio.com/events");
-  var sync = $firebase(ref);
+	var ref = new Firebase("https://heyscheduleme.firebaseio.com/events");
+	var sync = $firebase(ref);
 
-  $scope.events = sync.$asArray();
-  $scope.predicate = 'time';
+	$scope.events = sync.$asArray();
+	$scope.show = false;
+	$scope.upcomingEvent = {name: 'test', value: 'test', time: 9999};
 
-  $scope.addEvent = function(name, location, time) {
-    $scope.events.$add({name: name, location: location, time: time});
-  };
+
+	$scope.addEvent = function(name, location, time) {
+		$scope.events.$add({name: name, location: location, time: time});
+	};
+
+	$scope.checkUpcomingEvent = function() {
+		angular.forEach($scope.events, function(value, time) {
+			if ($scope.upcomingEvent.time > value.time) {
+				$scope.upcomingEvent = value;
+			}
+		});
+		$scope.show = true;
+	};
 
 });
